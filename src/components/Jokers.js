760d15c73d7df7955c5c860831aeff41b8769ad0
Omar Corbic <omar_corbic@hotmail.com>
2023-01-24 14:33:39 +0100

    Implemented useContex hook & cleaned up the code

--- src/components/Jokers.js
+++ src/components/Jokers.js
@@ -1,37 +1,57 @@
-import {FaPhoneAlt } from 'react-icons/fa';
-import {BsFillPeopleFill, BsHexagonHalf } from 'react-icons/bs';
-import { useEffect, useRef } from 'react';
+import { FaPhoneAlt } from "react-icons/fa";
+import { BsFillPeopleFill, BsHexagonHalf } from "react-icons/bs";
+import { useEffect, useRef } from "react";
+import { GameContext } from "./StartDisplay";
+import { useContext } from "react";
 
-const Jokers = ({ gameOn, onJokerCall }) =>{
+const Jokers = ({ onJokerCall }) => {
+  const { gameOn } = useContext(GameContext);
 
-    const friendBtnRef = useRef();
-    const fiftyBtnRef = useRef();
-    const audienceBtnRef = useRef();
+  const friendBtnRef = useRef();
+  const fiftyBtnRef = useRef();
+  const audienceBtnRef = useRef();
 
-    useEffect(() => {
-        if(!gameOn){
-            friendBtnRef.current.classList.remove('unclickable');
-            fiftyBtnRef.current.classList.remove('unclickable');
-            audienceBtnRef.current.classList.remove('unclickable');
-        }
-
-        //console.log('Jokers useEffect triggered!');
-    }, [gameOn])
-
-    const handleClick = (e) => {
-        if(!e.target.classList.contains('unclickable')){
-            e.target.classList.add('unclickable');
-        }
-        onJokerCall(e.target.id);
+  useEffect(() => {
+    if (!gameOn) {
+      friendBtnRef.current.classList.remove("unclickable");
+      fiftyBtnRef.current.classList.remove("unclickable");
+      audienceBtnRef.current.classList.remove("unclickable");
     }
 
-    return (
-        <div className="jokers-wrapper">
-            <button ref={friendBtnRef} id="FRIEND" onClick={(e) => handleClick(e)}> <FaPhoneAlt className='unclickable' color='white'/> </button>
-            <button ref={fiftyBtnRef} id="FIFTY_FIFTY" onClick={(e) => handleClick(e)}> <BsHexagonHalf className='unclickable' color='white'/> </button>
-            <button ref={audienceBtnRef} id="AUDIENCE" onClick={(e) => handleClick(e)}> <BsFillPeopleFill className='unclickable' color='white'/> </button>
-        </div>
-    )
-}
+    //console.log('Jokers useEffect triggered!');
+  }, [gameOn]);
+
+  const handleClick = (e) => {
+    if (!e.target.classList.contains("unclickable")) {
+      e.target.classList.add("unclickable");
+    }
+    onJokerCall(e.target.id);
+  };
+
+  return (
+    <div className="jokers-wrapper">
+      <button ref={friendBtnRef} id="FRIEND" onClick={(e) => handleClick(e)}>
+        {" "}
+        <FaPhoneAlt className="unclickable" color="white" />{" "}
+      </button>
+      <button
+        ref={fiftyBtnRef}
+        id="FIFTY_FIFTY"
+        onClick={(e) => handleClick(e)}
+      >
+        {" "}
+        <BsHexagonHalf className="unclickable" color="white" />{" "}
+      </button>
+      <button
+        ref={audienceBtnRef}
+        id="AUDIENCE"
+        onClick={(e) => handleClick(e)}
+      >
+        {" "}
+        <BsFillPeopleFill className="unclickable" color="white" />{" "}
+      </button>
+    </div>
+  );
+};
 
 export default Jokers;
--- src/components/StartDisplay.js
+++ src/components/StartDisplay.js
@@ -1,98 +1,113 @@
-import MainSection from './MainSection.js';
-import Ladder from './Ladder.js';
-import { useState, useEffect, useCallback } from 'react';
-import _ from 'lodash';
+import MainSection from "./MainSection.js";
+import Ladder from "./Ladder.js";
+import { useState, useEffect, useCallback } from "react";
+import _ from "lodash";
+import React from "react";
 
-const StartDisplay = () =>{
+export const GameContext = React.createContext();
 
-    const [questionsData, setQuestionsData] = useState([]);
-    const [gameOn, setGameOn] = useState(false);
-    const [level, setLevel] = useState(0);
-    const [isMillionaire, setIsMillionaire] = useState(false);
-    const [calledJoker, setCalledJoker] = useState('');
+const StartDisplay = () => {
+  const [questionsData, setQuestionsData] = useState([]);
+  const [gameOn, setGameOn] = useState(false);
+  const [level, setLevel] = useState(0);
+  const [isMillionaire, setIsMillionaire] = useState(false);
+  const [calledJoker, setCalledJoker] = useState("");
 
-    useEffect(() =>{
+  useEffect(() => {
+    const dataFetch = async () => {
+      const result = (
+        await Promise.all([
+          fetch(
+            "https://the-trivia-api.com/api/questions?limit=5&difficulty=medium"
+          ),
+          fetch(
+            "https://the-trivia-api.com/api/questions?limit=9&difficulty=hard"
+          ),
+        ])
+      ).map((r) => r.json());
 
-        const dataFetch = async () =>{
+      const [mediumQuestions, hardQuestions] = await Promise.all(result);
 
-            const result = (
-                await Promise.all([
-                    fetch("https://the-trivia-api.com/api/questions?limit=5&difficulty=medium"),
-                    fetch("https://the-trivia-api.com/api/questions?limit=9&difficulty=hard")
-                ])
-            ).map(r => r.json());
-
-            const [mediumQuestions, hardQuestions] = await Promise.all(result);
-
-            const allQuestions = [...mediumQuestions, ...hardQuestions].map( ({question, correctAnswer, incorrectAnswers}) => {
-
-                const shuffledAnswers = _.shuffle([correctAnswer, ...incorrectAnswers]).map((answer, index) => {
-                    return {
-                        id: String.fromCharCode(65 + index),
-                        text: answer
-                    }
-                });
-                return {
-                    question: question, 
-                    correctAnswer: correctAnswer, 
-                    shuffledAnswers: shuffledAnswers
-                };
-            });
-            
-            setQuestionsData([...allQuestions]);
+      const allQuestions = [...mediumQuestions, ...hardQuestions].map(
+        ({ question, correctAnswer, incorrectAnswers }) => {
+          const shuffledAnswers = _.shuffle([
+            correctAnswer,
+            ...incorrectAnswers,
+          ]).map((answer, index) => {
+            return {
+              id: String.fromCharCode(65 + index),
+              text: answer,
+            };
+          });
+          return {
+            question: question,
+            correctAnswer: correctAnswer,
+            shuffledAnswers: shuffledAnswers,
+          };
         }
-        
-        !gameOn && dataFetch();
+      );
 
-        return () =>{
-            setLevel(0);
-            setCalledJoker('');
-        }
-    }, [gameOn])
+      setQuestionsData([...allQuestions]);
+    };
 
-    useEffect(() => {
-        if(level === 14){
-            setIsMillionaire(true);
-        }
-    }, [level]);
+    !gameOn && dataFetch();
 
-    
-    const handleLevelChange = useCallback((num) =>{
-        setLevel(num);
-    }, []);
-    
-    const handleJokerCall = useCallback((joker) =>{
-        setCalledJoker(joker);
-    }, []);
-    
-    const handlePlayAgain = useCallback(() =>{
-        setGameOn(false);
-    }, []);
-    
-    const startGame = () =>{
-        setGameOn(true);
+    return () => {
+      setLevel(0);
+      setCalledJoker("");
+    };
+  }, [gameOn]);
+
+  useEffect(() => {
+    if (level === 14) {
+      setIsMillionaire(true);
     }
+  }, [level]);
 
-    if(!gameOn){
-        return (
+  const handleJokerCall = useCallback((joker) => {
+    setCalledJoker(joker);
+  }, []);
+
+  const startGame = () => {
+    setGameOn(true);
+  };
+
+  if (!gameOn) {
+    return (
+      <div className="start-display">
+        <button onClick={startGame} className="start-game-btn">
+          START GAME
+        </button>
+      </div>
+    );
+  } else {
+    if (isMillionaire) {
+      return (
         <div className="start-display">
-            <button onClick={startGame}className="start-game-btn">START GAME</button>
+          CONGRATULATIONS, YOU ARE A MILLIONAIRE!!!
         </div>
-        )
-    }else{
-        if(isMillionaire){
-            return(
-                <div className="start-display">CONGRATULATIONS, YOU ARE A MILLIONAIRE!!!</div>
-            )
-        }else{
-            return (
-                <div className="container">
-                    <MainSection questionsData={questionsData} onPlayAgain={handlePlayAgain} calledJoker={calledJoker} onLevelChange={handleLevelChange} />
-                    <Ladder gameOn={gameOn} level={level} onJokerCall={handleJokerCall}/>
-                </div>
-            )
-        }        
-    }   
-}
+      );
+    } else {
+      return (
+        <GameContext.Provider
+          value={{
+            level: level,
+            gameOn: gameOn,
+            setLevel: setLevel,
+            setGameOn: setGameOn,
+          }}
+        >
+          <div className="container">
+            <MainSection
+              questionsData={questionsData}
+              calledJoker={calledJoker}
+            />
+            <Ladder onJokerCall={handleJokerCall} />
+          </div>
+        </GameContext.Provider>
+      );
+    }
+  }
+};
 
 export default StartDisplay;
